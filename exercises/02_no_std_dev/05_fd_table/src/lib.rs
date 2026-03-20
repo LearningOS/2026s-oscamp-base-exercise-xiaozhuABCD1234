8f7b3dce3752bb317a929eb5e8633f916a958aca
xiaozhu <xiaozhuABCD1234@163.com>
2026-03-20 22:01:09 +0800

    feat: 完成02_no_std_dev

--- exercises/02_no_std_dev/05_fd_table/src/lib.rs
+++ exercises/02_no_std_dev/05_fd_table/src/lib.rs
@@ -51,13 +51,14 @@ pub struct FdTable {
     // TODO: Design the internal structure
     // Hint: use Vec<Option<Arc<dyn File>>>
     //       the index is the fd number, None means the fd is closed or unallocated
+    table: Vec<Option<Arc<dyn File>>>,
 }
 
 impl FdTable {
     /// Create an empty fd table
     pub fn new() -> Self {
         // TODO
-        todo!()
+        Self { table: Vec::new() }
     }
 
     /// Allocate a new fd, return the fd number.
@@ -65,25 +66,42 @@ impl FdTable {
     /// Prefers reusing the smallest closed fd number; if no free slot, appends to the end.
     pub fn alloc(&mut self, file: Arc<dyn File>) -> usize {
         // TODO
-        todo!()
+        for (fd, slot) in self.table.iter_mut().enumerate() {
+            if slot.is_none() {
+                *slot = Some(file);
+                return fd;
+            }
+        }
+        let fd = self.table.len();
+        self.table.push(Some(file));
+        fd
     }
 
     /// Get the file object for an fd. Returns None if the fd doesn't exist or is closed.
     pub fn get(&self, fd: usize) -> Option<Arc<dyn File>> {
         // TODO
-        todo!()
+        match self.table.get(fd) {
+            Some(slot) => slot.clone(),
+            None => None,
+        }
     }
 
     /// Close an fd. Returns true on success, false if the fd doesn't exist or is already closed.
     pub fn close(&mut self, fd: usize) -> bool {
         // TODO
-        todo!()
+        if let Some(slot) = self.table.get_mut(fd) {
+            if slot.is_some() {
+                *slot = None;
+                return true;
+            }
+        }
+        false
     }
 
     /// Return the number of currently allocated fds (excluding closed ones)
     pub fn count(&self) -> usize {
         // TODO
-        todo!()
+        self.table.iter().filter(|slot| slot.is_some()).count()
     }
 }
 
