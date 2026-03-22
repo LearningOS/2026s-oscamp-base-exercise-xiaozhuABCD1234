67f5659d28af247db8c7bcc6d5df19f58a0c2fb4
xiaozhu <xiaozhuABCD1234@163.com>
2026-03-22 23:30:07 +0800

    feat: 完成03_spinlock

--- exercises/03_os_concurrency/03_spinlock/src/lib.rs
+++ exercises/03_os_concurrency/03_spinlock/src/lib.rs
@@ -41,7 +41,17 @@ impl<T> SpinLock<T> {
     /// Caller must ensure `unlock` is called after using the data.
     pub fn lock(&self) -> &mut T {
         // TODO
-        todo!()
+        loop {
+            match self
+                .locked
+                .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            {
+                Ok(_) => return unsafe { &mut *self.data.get() },
+                Err(_) => {
+                    std::hint::spin_loop();
+                }
+            }
+        }
     }
 
     /// Release lock.
@@ -49,14 +59,20 @@ impl<T> SpinLock<T> {
     /// TODO: Set locked to false (using Release ordering)
     pub fn unlock(&self) {
         // TODO
-        todo!()
+        self.locked.store(false, Ordering::Relaxed);
     }
 
     /// Try to acquire lock without spinning.
     /// Returns Some(&mut T) on success, None if lock is busy.
     pub fn try_lock(&self) -> Option<&mut T> {
         // TODO: Single compare_exchange attempt
-        todo!()
+        match self
+            .locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+        {
+            Ok(_) => return unsafe { Some(&mut *self.data.get()) },
+            Err(_) => None,
+        }
     }
 }
 
