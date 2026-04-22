c89a26d6133b23288707980fbc8c43bb581f6ff6
xiaozhu <xiaozhuABCD1234@163.com>
2026-04-22 16:08:38 +0000

    feat: 完成02_tokio_tasks

--- exercises/05_async_programming/02_tokio_tasks/src/lib.rs
+++ exercises/05_async_programming/02_tokio_tasks/src/lib.rs
@@ -17,7 +17,19 @@ pub async fn concurrent_squares(n: usize) -> Vec<usize> {
     // TODO: Create n asynchronous tasks, each computing i * i
     // TODO: Collect all JoinHandle
     // TODO: Await each one to get result
-    todo!()
+    let mut handles: Vec<JoinHandle<usize>> = Vec::with_capacity(n);
+
+    for i in 0..n {
+        let handle = tokio::spawn(async move { i * i });
+        handles.push(handle);
+    }
+
+    let mut results = vec![];
+    for handle in handles {
+        let result = handle.await.unwrap();
+        results.push(result);
+    }
+    results
 }
 
 /// Concurrently execute multiple "time-consuming" tasks (simulated with sleep), return all results.
@@ -28,7 +40,24 @@ pub async fn parallel_sleep_tasks(n: usize, duration_ms: u64) -> Vec<usize> {
     // TODO: Create asynchronous task for each id in 0..n
     // TODO: Each task sleeps specified duration and returns its own id
     // TODO: Collect all results and sort
-    todo!()
+    let mut handles: Vec<JoinHandle<usize>> = Vec::with_capacity(n);
+
+    for i in 0..n {
+        let handle = tokio::spawn(async move {
+            sleep(Duration::from_millis(duration_ms)).await;
+            i
+        });
+        handles.push(handle);
+    }
+
+    let mut results = Vec::with_capacity(n);
+    for handle in handles {
+        let result = handle.await.unwrap();
+        results.push(result);
+    }
+
+    results.sort_unstable();
+    results
 }
 
 #[cfg(test)]
